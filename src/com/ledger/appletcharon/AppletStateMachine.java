6cfd7035514b0c68d549cc9738fa11ece95d91f6
Alexis Grojean <alexis.grojean@ledger.fr>
2025-01-21 10:02:14 +0100

    Refactor state machines with more strict state management.

--- src/com/ledger/appletcharon/AppletStateMachine.java
+++ src/com/ledger/appletcharon/AppletStateMachine.java
@@ -1,5 +1,9 @@
 package com.ledger.appletcharon;
 
+import javacard.framework.ISO7816;
+import javacard.framework.ISOException;
+import javacard.framework.JCSystem;
+
 public class AppletStateMachine {
     // Constants for states
     public static final byte STATE_FABRICATION = 0;
@@ -15,30 +19,72 @@ public class AppletStateMachine {
     private byte currentState;
 
     public AppletStateMachine() {
-        currentState = STATE_FABRICATION;
+        setCurrentState(STATE_FABRICATION);
+    }
+
+    private boolean isValidState(byte state) {
+        switch (state) {
+        case STATE_FABRICATION:
+        case STATE_ATTESTED:
+        case STATE_USER_PERSONALIZED:
+            return true;
+        default:
+            return false;
+        }
+    }
+
+    private void setCurrentState(byte newState) {
+        JCSystem.beginTransaction();
+        try {
+            if (!isValidState(newState)) {
+                JCSystem.abortTransaction();
+                // TODO: implement "fatal error". This should never happen.
+                ISOException.throwIt(ISO7816.SW_DATA_INVALID);
+            }
+
+            currentState = newState;
+            JCSystem.commitTransaction();
+        } catch (Exception e) {
+            JCSystem.abortTransaction();
+            throw e;
+        }
     }
 
     public void transition(byte event) {
+        byte newState = currentState;
+
         switch (currentState) {
         case STATE_FABRICATION:
             if (event == EVENT_SET_CERTIFICATE) {
-                currentState = STATE_ATTESTED;
+                newState = STATE_ATTESTED;
             }
             break;
         case STATE_ATTESTED:
             if (event == EVENT_SET_SEED) {
-                currentState = STATE_USER_PERSONALIZED;
+                newState = STATE_USER_PERSONALIZED;
             }
             break;
         case STATE_USER_PERSONALIZED:
             if (event == EVENT_PIN_TRY_LIMIT_EXCEEDED || event == EVENT_FACTORY_RESET) {
-                currentState = STATE_ATTESTED;
+                newState = STATE_ATTESTED;
             }
             break;
+        default:
+            // TODO: implement "fatal error". This should never happen.
+            ISOException.throwIt(ISO7816.SW_DATA_INVALID);
+            break;
+        }
+
+        if (newState != currentState) {
+            setCurrentState(newState);
         }
     }
 
     public byte getCurrentState() {
+        if (!isValidState(currentState)) {
+            // TODO: implement "fatal error". This should never happen.
+            ISOException.throwIt(ISO7816.SW_DATA_INVALID);
+        }
         return currentState;
     }
 }

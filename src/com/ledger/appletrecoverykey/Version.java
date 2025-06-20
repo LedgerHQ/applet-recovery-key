79dd83c3de1677ced9662832e8e84731ddd8fb06
Alexis Grojean <alexis.grojean@ledger.fr>
2025-06-20 13:32:29 +0200

    Update version to 99.99 - FOR TESTING ONLY

--- src/com/ledger/appletrecoverykey/Version.java
+++ src/com/ledger/appletrecoverykey/Version.java
@@ -6,8 +6,8 @@
 package com.ledger.appletrecoverykey;
 
 public final class Version {
-    protected static final byte APPLET_MAJOR_VERSION = (byte) 0x01;
-    protected static final byte APPLET_MINOR_VERSION = (byte) 0x00;
+    protected static final byte APPLET_MAJOR_VERSION = (byte) 0x63;
+    protected static final byte APPLET_MINOR_VERSION = (byte) 0x63;
     // CAP file version format does not support patch version
     // Beware if you update the patch, it means the CAP file version (major.minor)
     // will still remain unchanged and it won't reflect the changes in the
